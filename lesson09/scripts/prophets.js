7e7c9cd68d400c78f8576b171ad973e0e6360a38
SamanthaSortomme <sor23001@byui.edu>
2023-02-06 18:49:27 -0800

    prophets not working

--- lesson09/scripts/prophets.js
+++ lesson09/scripts/prophets.js
@@ -14,26 +14,34 @@ getProphetData();
 const displayProphets = (prophets) => {
     const cards = document.querySelector('div.cards'); // select the output container element
 
-    prophets.forEach((prophet) => {
+    prophets.forEach(prophet => {
         // Create elements to add to the div.cards element
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
         let portrait = document.createElement('img');
+        let birth = document.createElement('p');
+        let place = document.createElement('p');
+
 
         // Build the h2 content out to show the prophet's full name - finish the template string
-        h2.textContent = `${prophet.name} ____________`;
+        h2.textContent = `${prophet.name} ${prophet.lastname}`;
 
         // Build the image portrait by setting all the relevant attribute
         portrait.setAttribute('src', prophet.imageurl);
-        portrait.setAttribute('alt', `Portait of ${prophet.name} ______________`);
+        portrait.setAttribute('alt', `Portait of ${prophet.name} ${prophet.lastname}`);
+
         portrait.setAttribute('loading', 'lazy');
         portrait.setAttribute('width', '340');
         portrait.setAttribute('height', '440');
+        // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        birth.textContent = `${prophet.birthdate}`;
+        place.textContent = `${prophet.birthplace}`
 
         // Append the section(card) with the created elements
         card.appendChild(h2);
+        card.appendChild(birth);
+        card.appendChild(place);
         card.appendChild(portrait);
-
         cards.appendChild(card);
     }) // end of forEach loop
 } // end of function expression
